667854970025caa43ded488fa50db6acdcbe9e53
N4WEEDAH <navid.ahmad@dcmail.ca>
2025-04-04 19:23:48 -0400

    Update main.js

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -46,4 +46,7 @@ function generateStory() {
         newStory = newStory.replace("300 pounds", weight);
         newStory = newStory.replace("94 fahrenheit", temperature);
     }
-    
+        // print
+    story.textContent = newStory;
+    story.style.visibility = "visible";
+}
