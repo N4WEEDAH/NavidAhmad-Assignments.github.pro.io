68370c0d5a607c8c1dedb6dab842ed23d80fffe4
N4WEEDAH <navid.ahmad@dcmail.ca>
2025-04-04 19:22:30 -0400

    commit

---
+++ lab4/part1/main.js
@@ -0,0 +1,27 @@
+// 1.  elements
+const customName = document.getElementById("customname");
+const randomize = document.querySelector(".randomize");
+const story = document.querySelector(".story");
+
+// 2. random value from  array
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+// 3. Story
+const storyText =
+    "It was 94 fahrenheit outside, so :insertx: went for a walk. " +
+    "When they got to :inserty:, they stared in horror for a few moments, " +
+    "then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: " +
+    "weighs 300 pounds, and it was a hot day.";
+
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+// 4.  listener for button
+randomize.addEventListener("click", generateStory);
+
+function generateStory() {
+    let newStory = storyText;
+    
