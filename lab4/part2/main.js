6fad749ff804013655a8ae84b525c9de31ba4aa9
N4WEEDAH <navid.ahmad@dcmail.ca>
2025-04-04 20:08:03 -0400

    Update main.js

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -1,12 +1,19 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFiles = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altTexts = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'Rock formations on a beach',
+    'pic3.jpg': 'Purple flowers',
+    'pic4.jpg': 'Ancient temple',
+    'pic5.jpg': 'Raindrops on a leaf'
+  };
 
 /* Looping through images */
 
