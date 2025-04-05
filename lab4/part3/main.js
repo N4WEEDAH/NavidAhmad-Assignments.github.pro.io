70612050186400759102c04b8295b31be19e7686
N4WEEDAH <navid.ahmad@dcmail.ca>
2025-04-04 20:56:45 -0400

    Update main.js

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -17,3 +17,35 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+// Ball class definition
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  // Draw the ball on the canvas
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  // Update the ball's position and handle wall collisions
+  update() {
+    if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+      this.velX = -this.velX;
+    }
+    if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+      this.velY = -this.velY;
+    }
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
